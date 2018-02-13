fefbe7a1627ddaf80ae6f0d2de116e9fa72a4675
Armel Moth <armel.ghis.moth@gmail.com>
2018-02-13 11:41:21 +0100

    first commit

---
+++ R/aaa.R
@@ -0,0 +1,42 @@
+#' Adds the content of www to sF/
+#' 
+#' @importFrom shiny addResourcePath
+#' 
+#' @noRd
+#' 
+# .onLoad <- function(...) {
+#     addResourcePath('sF', system.file('www', package='shinyFilesDropBox'))
+# }
+addResourcePath('sF', '~/Documents/shinyFilesDropBox/inst/www')
+#' Run a simple example app using the shinyFiles functionality
+#' 
+#' When the function is invoked a shiny app is started showing a very simple 
+#' setup using shinyFiles. A button summons the dialog box allowing the user to
+#' navigate the R installation directory. To showcase the restrictions parameter
+#' the base package location has been hidden, and is thus inaccecible. A panel 
+#' besides the button shows how the user selection is made accessible to the 
+#' server after parsing with \code{\link{parseFilePaths}}.
+#' 
+#' @family shinyFiles
+#' 
+#' @importFrom shiny runApp
+#' 
+#' @export
+#' 
+shinyDropFilesExample <- function() {
+    runApp(system.file('example', package='shinyFilesDropBox', mustWork=T), display.mode='showcase')
+}
+
+
+getDropSession <- function() {
+    session <- shiny::getDefaultReactiveDomain()
+    
+    if (is.null(session)) {
+        stop(paste(
+            "could not find the Shiny session object. This usually happens when a",
+            "shinyjs function is called from a context that wasn't set up by a Shiny session."
+        ))
+    }
+    
+    session
+}
